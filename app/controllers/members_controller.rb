3202ef070e1096860d81e58491c87e4f34cb72b5
Cheng Pham <chengpham@gmail.com>
2021-01-27 18:06:51 -0800

    update

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,21 +2,10 @@ class MembersController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        puts "Members:---------------------#{member_params[:members_name]}"
-        puts "project:---------------------#{params[:project_id]}"
         member_params[:members_name].split(',').each do |email|
             user=User.find_by_email(email)
             Member.create(project_id: params[:project_id], user_id: user.id)
         end
-        # user = User.where(member_params[:users])
-        # puts "User:---------------------#{user}"
-        # user.each do |u|
-        #     puts "User:---------------------#{u}"
-        # end
-        # @member = Member.new member_params
-        # @member.project = @project
-        # @member.user = current_user
-        # @member.save
         redirect_to project_path(params[:project_id]), notice: "member added"
     end
     def destroy
@@ -29,7 +18,3 @@ class MembersController < ApplicationController
         params.require(:member).permit(:members_name)
     end
 end
-
-
-
-User.where("first_name ILIKE ? OR last_name ILIKE ?", "%rick%","%rick% ")
